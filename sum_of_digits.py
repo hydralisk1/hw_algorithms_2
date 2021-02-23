5bbaf5210a4b2ea51b074896ad8fa7971aab5b0b
hydralisk1 <hydralisk.ks@gmail.com>
2021-02-23 02:28:20 -0500

    HW_Algorithms_2

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -10,7 +10,21 @@ def sum_of_digits(n):
     
     return result if result < 10 else sum_of_digits(result)
 
+# Get the result using convert the element of this function from int to str
+def sum_of_digits_str(n):
+    if type(n) is not int or n <= 0:
+        raise ValueError("The element has to be one of the natural numbers")
+    
+    result = sum([int(c) for c in str(n)])
+    
+    return result if result < 10 else sum_of_digits_str(result)
+
 assert sum_of_digits(16) == 7
 assert sum_of_digits(942) == 6
 assert sum_of_digits(132189) == 6
 assert sum_of_digits(493193) == 2
+
+assert sum_of_digits_str(16) == 7
+assert sum_of_digits_str(942) == 6
+assert sum_of_digits_str(132189) == 6
+assert sum_of_digits_str(493193) == 2
--- zeros_not_for_heros.py
+++ zeros_not_for_heros.py
@@ -1,14 +1,32 @@
 def remove_zeros(n):
     if type(n) is not int:
         raise ValueError("The element has to be an integer number")
-    
-    while n % 10 == 0 and n != 0:
-        n //= 10
 
-    return n
+    return n if n == 0 or n % 10 != 0 else remove_zeros(n // 10)
+
+# Get the result using convert the element from int to str
+def remove_zeros_str(n):
+    if type(n) is not int:
+        raise ValueError("The element has to be an integer number")
+    
+    if n == 0:
+        return n
+
+    n = str(n)
+    for c in n[::-1]:
+        if c == "0":
+            n = n[:-1]
+        else:
+            return int(n)
 
 assert remove_zeros(1450) == 145
 assert remove_zeros(96000) == 96
 assert remove_zeros(1050) == 105
 assert remove_zeros(-1050) == -105
 assert remove_zeros(0) == 0
+
+assert remove_zeros_str(1450) == 145
+assert remove_zeros_str(96000) == 96
+assert remove_zeros_str(1050) == 105
+assert remove_zeros_str(-1050) == -105
+assert remove_zeros_str(0) == 0
